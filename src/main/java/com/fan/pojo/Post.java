d4f90ef9f10f8b9c7444aca0b5b4ee5adab78d2f
fanstudy <2864169033@qq.com>
2021-11-27 11:38:05 +0800

    demo

---
+++ src/main/java/com/fan/pojo/Post.java
@@ -0,0 +1,25 @@
+package com.fan.pojo;
+
+
+import lombok.Data;
+
+@Data
+public class Post {
+    private int postId;
+    private String title ;
+    private String text;
+    private String  category;
+    private User user;
+
+    public Post() {
+
+    }
+
+    public Post(int postId, String title, String text, String category) {
+        this.postId = postId;
+        this.title = title;
+        this.text = text;
+        this.category = category;
+    }
+
+}
---
+++ src/main/java/com/fan/service/PostService.java
@@ -0,0 +1,24 @@
+package com.fan.service;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.fan.pojo.Post;
+import com.fan.pojo.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService implements GraphQLQueryResolver {
+    /**
+     * 为了测试，只查询id为1的结果
+     */
+    public Post getPostById(int id) {
+        if (id == 1) {
+            User user = new User(1, "javadaily", "JAVA日知录", "zhangsan@qq.com");
+            Post post = new Post(1, "Hello,Graphql", "Graphql初体验", "日记");
+            post.setUser(user);
+            return post;
+        } else {
+            return null;
+        }
+
+    }
+}
